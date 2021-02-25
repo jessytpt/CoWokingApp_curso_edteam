82a9051952ef52f71e24dd682c91235f42ce7e99
jessytpt <jessytpt.dev@gmail.com>
2021-02-24 21:50:03 -0500

    Creación menú de administrador

--- CoworkingApp.App/Program.cs
+++ CoworkingApp.App/Program.cs
@@ -7,10 +7,82 @@ namespace CoworkingApp.App
     {
         static void Main(string[] args)
         {
-            // var userData = new UserData();
-            // userData.CreateAdmin();       
-            Console.WriteLine("1 = Administrados; 2 = Usuario ");
-            // esta es otra rama 
+            string roleSelected = "";
+            Console.WriteLine("\nBienvenido al CoWorking!\n");   
+            while (roleSelected!="1" && roleSelected!="2")
+            {
+                Console.WriteLine("1 = Administrados; 2 = Usuario ");   
+                roleSelected = Console.ReadLine();    
+            }       
+            
+            if(roleSelected=="1"){
+                string menuAdminSelected = "";
+                while (menuAdminSelected!="1" && menuAdminSelected!="2")
+                {
+                    Console.WriteLine("1 = Administración de Puestos; 2 = Administración de Usuarios ");   
+                    menuAdminSelected = Console.ReadLine();    
+                } 
+                if (menuAdminSelected=="1")
+                {
+                    string menuPuestosSelected = "";
+                    while(menuPuestosSelected!="1" && 
+                          menuPuestosSelected!="2" &&
+                          menuPuestosSelected!="3" &&
+                          menuPuestosSelected!="4" )
+                    {
+                              Console.WriteLine("Administración de puestos");
+                        Console.WriteLine("1=Crear; 2=Editar; 3=Eliminar; 4=Bloquear");
+                        menuPuestosSelected = Console.ReadLine();   
+                    }
+                    
+                    switch (menuPuestosSelected)
+                    {
+                        case "1":
+                            Console.WriteLine("Opción: crear puesto");
+                        break;
+                        case "2":
+                            Console.WriteLine("Opción: editar puesto");
+                        break;
+                        case "3":
+                            Console.WriteLine("Opción: eliminar puesto");
+                        break;
+                        case "4":
+                            Console.WriteLine("Opción: bloquear puesto");
+                        break;
+                    } 
+                }else if (menuAdminSelected=="2")
+                {
+                    string menuUsuarioSelected = "";
+                    while(menuUsuarioSelected!="1" && 
+                          menuUsuarioSelected!="2" &&
+                          menuUsuarioSelected!="3" &&
+                          menuUsuarioSelected!="4" )
+                    {
+                              Console.WriteLine("Administración de usuarios");
+                        Console.WriteLine("1=Crear; 2=Editar; 3=Eliminar; 4=Cambiar contraseña");
+                        menuUsuarioSelected = Console.ReadLine();   
+                    }
+                    
+                    switch (menuUsuarioSelected)
+                    {
+                        case "1":
+                            Console.WriteLine("Opción: crear usuario");
+                        break;
+                        case "2":
+                            Console.WriteLine("Opción: editar usuario");
+                        break;
+                        case "3":
+                            Console.WriteLine("Opción: eliminar usuario");
+                        break;
+                        case "4":
+                            Console.WriteLine("Opción: cambiar contraseña");
+                        break;
+                    } 
+                }
+            }else if(roleSelected=="2")
+            {
+                
+            }
         }
     }
 }
